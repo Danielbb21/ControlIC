608580c9d53f7bbbac6c477e052b57cd072cd60c
be3013 <57907227+be3013@users.noreply.github.com>
2020-10-19 13:47:52 -0300

    Imagem padrão para usuario sem perfil e mudança no layout de user page

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -302,11 +302,18 @@ namespace ControlIC.Controllers {
                 return NotFound();
             }
 
-            string imreBase64Data = Convert.ToBase64String(usuario.ImgUsuario);
-            string imgDataURL = string.Format("data:image/png;base64,{0}", imreBase64Data);
-            //Passing image data in viewbag to view  
-            ViewBag.ImageData = imgDataURL;
-
+            if(usuario.ImgUsuario == null) 
+            {
+                ViewBag.ImageData = "/Imagens/Placeholder_Perfil.png";
+            }
+            else 
+            {
+                string imreBase64Data = Convert.ToBase64String(usuario.ImgUsuario);
+                string imgDataURL = string.Format("data:image/png;base64,{0}", imreBase64Data);
+                //Passing image data in viewbag to view  
+                ViewBag.ImageData = imgDataURL;
+            }
+            
             if (usuario.TipoUsuario == 1) 
             {
                 ViewBag.Especifico = "Curso";
